7b71bdbd6d49102609c34d374922ac8668cf4072
Vama Trivedi <vama.trivedi1994@gmail.com>
2018-07-31 09:24:35 -0700

    adding files

---
+++ pages/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { DataSearch } from "@appbaseio/reactivesearch";
+
+const components = {
+  dataSearch: {
+    componentId: "mainSearch",
+    dataField: "original_title.search",
+    categoryField: "title",
+    className: "search-bar",
+    queryFormat: "and",
+    placeholder: "Search for movies...",
+    iconPosition: "left",
+    autosuggest: false,
+    filterLabel: "search",
+    highlight: true
+  }
+};
+
+class Navbar extends Component {
+  static async getInitialProps() {
+    return {
+      store: await initReactivesearch(
+        [
+          {
+            ...components.datasearch,
+            type: "DataSearch",
+            source: DataSearch
+          }
+        ],
+        null
+      )
+    };
+  }
+  render() {
+    return (
+      <div className="navbar">
+        <div className="logo-container">
+          <img className="app-logo" src="/static/logo.jpg" alt="MovieBee" />
+        </div>
+
+        <div className="search-container">
+          <DataSearch {...components.dataSearch} />
+        </div>
+      </div>
+    );
+  }
+}
+export default Navbar;
---
+++ pages/_document.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { extractCritical } from "emotion-server";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    // for emotion-js
+    const page = renderPage();
+    const styles = extractCritical(page.html);
+    return { ...page, ...styles };
+  }
+
+  constructor(props) {
+    // for emotion-js
+    super(props);
+    const { __NEXT_DATA__, ids } = props;
+    if (ids) {
+      __NEXT_DATA__.ids = ids;
+    }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content="#000000" />
+          <title>MovieBee</title>
+
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossOrigin="anonymous"
+          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <style>{`body {   background-color: #00304e;
+            margin: 0px;
+            padding: 0px;
+            font-family: sans-serif;
+            font-size: 20px;}`}</style>
+          <link rel="manifest" href="static/manifest.json" />
+          <link rel="shortcut icon" href="static/favicon.ico" />
+
+          {/* for emotion-js */}
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
